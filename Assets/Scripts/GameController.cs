466076dd1f3edffd7e87ade3feef4f44398e6b2e
Deusald <Deusald@gmail.com>
2016-11-02 20:01:21 +0100

    Zajęcia 1
    Na zajęciach 1 wkonaliśmy generyczną planszę.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public int          lvlSize;
+    public GameObject   hardWall;
+    public GameObject   floor;
+
+    void Awake()
+    {
+        lvlSize = 5;
+    }
+
+    void Start()
+    {
+        CreateLvl();
+    }
+
+    void CreateLvl()
+    {
+        for(int i = 0; i < lvlSize; ++i)
+        {
+            Instantiate(hardWall, new Vector3(14f + i, 0f, 6f), Quaternion.identity);
+            Instantiate(hardWall, new Vector3(14f + i, 0f, -6f), Quaternion.identity);
+            Instantiate(floor, new Vector3(14f + i, -0.5f, 0f), Quaternion.identity);
+
+            if(i % 2 == 0)
+            {
+                for(int z = 4; z > -5; z -= 2)
+                    Instantiate(hardWall, new Vector3(14f + i, 0f, z), Quaternion.identity);
+            }
+        }
+
+        float x = lvlSize + 14;
+
+        for(int i = 0; i < 13; ++i)
+        {
+            Instantiate(hardWall, new Vector3(x, 0f, 6f - i), Quaternion.identity);
+        }
+    }
+}
